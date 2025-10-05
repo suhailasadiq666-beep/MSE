98de426ab6d6d9fb3e49acca86c78468e76fedf0
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-05 17:50:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import streamlit as st
+
+if "head" not in st.session_state:
+  st.session_state.head = True
+
+def start_exam():
+  st.session_state.head = False
+
+start = st.button("start exam" ,onclick = start_exam)
+
+st.title("MSE Maths and Scince Exam")
+if st.session_state.head:
+  st.header("click start exam to start the exam")
+
+
+
