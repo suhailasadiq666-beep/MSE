59a2bd86074123468700550fc753c73d6a094c0b
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-05 18:51:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,64 @@ import streamlit as st
 
 if "head" not in st.session_state:
   st.session_state.head = True
+if "rev_ans" not in st.session_state:
+  st.session_state.rev_ans = False
+
+points = 0
 
 def start_exam():
   st.session_state.head = False
+def reveal_ans():
+  st.session_state.rev_ans = True
 
 st.title("MSE Maths and Science Exam")
 if st.session_state.head:
-  st.write("click start exam to start the exam")
+  st.write(["click start exam to start the exam", "correct = +4      wrong = -1])
   start = st.button("start exam" ,on_click = start_exam)
 else:
-  st.markdown("#### science")
-  st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None)
-  st.markdown("---")
-  st.radio("Which organelle is often referred to as the 'powerhouse' of the cell because it generates most of the cell's supply of ATP (Adenosine Triphosphate)?", ["A) Nucleus","B) Mitochondrion","C) Endoplasmic Reticulum","D) Golgi Apparatus"], index = None)  
-  st.markdown("---")
-  st.radio("What is the chemical symbol for the element Gold?", ["A) Ag","B) Fe","C) Pt","D) Au"], index = None)    
-  st.markdown("---")
-  st.radio("What process involves the movement of water from the Earth's surface into the atmosphere, primarily as water vapor?", ["A) Condensation","B) Precipitation","C) Infiltration","D) Evaporation"], index = None)
-  st.markdown("---")
-  st.radio("Which of the following is the name given to a small, rocky body orbiting the Sun, typically found in the region between Mars and Jupiter?", ["A) Comet","B) Meteoroid","C) Asteroid","D) Planetoid"], index = None)
+  if not st.session_state.rev_ans:
+    st.markdown("#### science")
+    q1 = st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None)
+    st.markdown("---")
+    q2 = st.radio("Which organelle is often referred to as the 'powerhouse' of the cell because it generates most of the cell's supply of ATP (Adenosine Triphosphate)?", ["A) Nucleus","B) Mitochondrion","C) Endoplasmic Reticulum","D) Golgi Apparatus"], index = None)  
+    st.markdown("---")
+    q3 = st.radio("What is the chemical symbol for the element Gold?", ["A) Ag","B) Fe","C) Pt","D) Au"], index = None)    
+    st.markdown("---")
+    q4 = st.radio("What process involves the movement of water from the Earth's surface into the atmosphere, primarily as water vapor?", ["A) Condensation","B) Precipitation","C) Infiltration","D) Evaporation"], index = None)
+    st.markdown("---")
+    q5 = st.radio("Which of the following is the name given to a small, rocky body orbiting the Sun, typically found in the region between Mars and Jupiter?", ["A) Comet","B) Meteoroid","C) Asteroid","D) Planetoid"], index = None)
+    st.markdown("---")
+  if q1 == "C) Gravitational Force":
+    point += 4
+  else:
+    point -= 1
+  if q2 == "B) Mitochondrion":
+    point += 4
+  else:
+    point -= 1
+  if q3 == "D) Au":
+    point += 4
+  else:
+    point -= 1
+  if q4 == "D) Evaporation":
+    point += 4
+  else:
+    point -= 1
+  if q1 == "C) Gravitational Force":
+    point += 4
+  else:
+    point -= 1
+  if q5 == "C) Asteroid":
+    point += 4
+  else:
+    point -= 1
 
-
+  revealans = st.button("submit", on_click = reveal_ans)
+  
+  
+  
+    
+  
 
 
 
