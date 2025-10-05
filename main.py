72af51323ae01d4bad774b2fe9dff9bddbc6fb8f
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-05 18:08:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if st.session_state.head:
   st.write("click start exam to start the exam")
   start = st.button("start exam" ,on_click = start_exam)
 else:
-  st.markdown(####"science")
+  st.markdown("#### science")
   st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"])    
 
 
