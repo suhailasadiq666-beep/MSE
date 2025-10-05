ce9f3a2c1b5a0806d6b16c6aa51b66f2cf6efe18
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-05 18:06:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ if "head" not in st.session_state:
 def start_exam():
   st.session_state.head = False
 
-st.title("MSE Maths and Scince Exam")
+st.title("MSE Maths and Science Exam")
 if st.session_state.head:
   st.write("click start exam to start the exam")
+  start = st.button("start exam" ,on_click = start_exam)
 else:
-  st.write("scince")
-  st.radio("what is 2", ["VX","WFD","VFC",])
+  st.markdown(####"science")
+  st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"])    
 
-start = st.button("start exam" ,on_click = start_exam)
+
+
 
 
 
