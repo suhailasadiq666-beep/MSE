b1a6edf6684d7cd0b072c5b34b36dcd1d371e293
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-05 19:25:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ points = 0
 def start_exam():
   st.session_state.head = False
 def reveal_ans():
-  st.session_state.rev_ans = True
+  q1 = st.radio("",[])
+  q2 = st.radio("",[])
+  q3 = st.radio("",[])
+  q4 = st.radio("",[])
+  q5 = st.radio("",[])
+  
 
 st.title("MSE Maths and Science Exam")
 if st.session_state.head:
@@ -19,42 +24,40 @@ if st.session_state.head:
   st.write("wrong answer :  -1 points")
   start = st.button("start exam" ,on_click = start_exam)
 else:
-  if not st.session_state.rev_ans:
-    st.markdown("#### science")
-    q1 = st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None)
-    st.markdown("---")
-    q2 = st.radio("Which organelle is often referred to as the 'powerhouse' of the cell because it generates most of the cell's supply of ATP (Adenosine Triphosphate)?", ["A) Nucleus","B) Mitochondrion","C) Endoplasmic Reticulum","D) Golgi Apparatus"], index = None)  
-    st.markdown("---")
-    q3 = st.radio("What is the chemical symbol for the element Gold?", ["A) Ag","B) Fe","C) Pt","D) Au"], index = None)    
-    st.markdown("---")
-    q4 = st.radio("What process involves the movement of water from the Earth's surface into the atmosphere, primarily as water vapor?", ["A) Condensation","B) Precipitation","C) Infiltration","D) Evaporation"], index = None)
-    st.markdown("---")
-    q5 = st.radio("Which of the following is the name given to a small, rocky body orbiting the Sun, typically found in the region between Mars and Jupiter?", ["A) Comet","B) Meteoroid","C) Asteroid","D) Planetoid"], index = None)
-    st.markdown("---")
-    if q1 == "C) Gravitational Force":
-      points += 4
-    else:
-      points -= 1
-    if q2 == "B) Mitochondrion":
-      points += 4
-    else:
-      points -= 1
-    if q3 == "D) Au":
-      points += 4
-    else:
-      points -= 1
-    if q4 == "D) Evaporation":
-      points += 4
-    else:
-      points -= 1
-    if q5 == "C) Asteroid":
-      points += 4
-    else:
-      points -= 1
-    total = points
-    revealans = st.button("submit", on_click = reveal_ans)
+  st.markdown("#### science")
+  q1 = st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None)
+  st.markdown("---")
+  q2 = st.radio("Which organelle is often referred to as the 'powerhouse' of the cell because it generates most of the cell's supply of ATP (Adenosine Triphosphate)?", ["A) Nucleus","B) Mitochondrion","C) Endoplasmic Reticulum","D) Golgi Apparatus"], index = None)  
+  st.markdown("---")
+  q3 = st.radio("What is the chemical symbol for the element Gold?", ["A) Ag","B) Fe","C) Pt","D) Au"], index = None)    
+  st.markdown("---")
+  q4 = st.radio("What process involves the movement of water from the Earth's surface into the atmosphere, primarily as water vapor?", ["A) Condensation","B) Precipitation","C) Infiltration","D) Evaporation"], index = None)
+  st.markdown("---")
+  q5 = st.radio("Which of the following is the name given to a small, rocky body orbiting the Sun, typically found in the region between Mars and Jupiter?", ["A) Comet","B) Meteoroid","C) Asteroid","D) Planetoid"], index = None)
+  st.markdown("---")
+  if q1 == "C) Gravitational Force":
+    points += 4
   else:
-    st.markdown(f"### you got {st.session_state.rev_ans.total} points")
+    points -= 1
+  if q2 == "B) Mitochondrion":
+    points += 4
+  else:
+    points -= 1
+  if q3 == "D) Au":
+    points += 4
+  else:
+    points -= 1
+  if q4 == "D) Evaporation":
+    points += 4
+  else:
+    points -= 1
+  if q5 == "C) Asteroid":
+    points += 4
+  else:
+    points -= 1
+  total = points
+  revealans = st.button("submit", on_click = reveal_ans)
+  st.markdown(f"### you got {} points")
   
   
   
