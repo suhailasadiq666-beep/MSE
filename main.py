c380744d30c96af139ab7a4af91749ec26da5718
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-05 17:53:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if "head" not in st.session_state:
 def start_exam():
   st.session_state.head = False
 
-start = st.button("start exam" ,onclick = start_exam)
+start = st.button("start exam" ,on_click = start_exam)
 
 st.title("MSE Maths and Scince Exam")
 if st.session_state.head:
