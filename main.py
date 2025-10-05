51fa3bd0147ed4700fb7dfccf29a3ed8bef11262
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-05 18:13:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ if st.session_state.head:
   start = st.button("start exam" ,on_click = start_exam)
 else:
   st.markdown("#### science")
-  st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None)    
-
+  st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None)
+  st.radio("Which organelle is often referred to as the "powerhouse" of the cell because it generates most of the cell's supply of ATP (Adenosine Triphosphate)?", ["A) Nucleus","B) Mitochondrion","C) Endoplasmic Reticulum","D) Golgi Apparatus"], index = None)    
+  
 
 
 
