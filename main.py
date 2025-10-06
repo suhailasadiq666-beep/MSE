369fbaf6b37a0e02789c5f8f4b6e72ca594d6278
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-06 14:00:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ points = 0
 
 def start_exam():
   st.session_state.head = False
-def reveal_ans():
-  st.clear()
+
+place = st.empty
+
   
 
 st.title("MSE Maths and Science Exam")
@@ -20,40 +21,42 @@ if st.session_state.head:
   st.write("wrong answer :  -1 points")
   start = st.button("start exam" ,on_click = start_exam)
 else:
-  st.markdown("#### science")
-  q1 = st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None, key = "q1")
-  st.markdown("---")
-  q2 = st.radio("Which organelle is often referred to as the 'powerhouse' of the cell because it generates most of the cell's supply of ATP (Adenosine Triphosphate)?", ["A) Nucleus","B) Mitochondrion","C) Endoplasmic Reticulum","D) Golgi Apparatus"], index = None, key = "q2")  
-  st.markdown("---")
-  q3 = st.radio("What is the chemical symbol for the element Gold?", ["A) Ag","B) Fe","C) Pt","D) Au"], index = None, key = "q3")    
-  st.markdown("---")
-  q4 = st.radio("What process involves the movement of water from the Earth's surface into the atmosphere, primarily as water vapor?", ["A) Condensation","B) Precipitation","C) Infiltration","D) Evaporation"], index = None, key = "q4")
-  st.markdown("---")
-  q5 = st.radio("Which of the following is the name given to a small, rocky body orbiting the Sun, typically found in the region between Mars and Jupiter?", ["A) Comet","B) Meteoroid","C) Asteroid","D) Planetoid"], index = None, key = "q5")
-  st.markdown("---")
-  if q1 == "C) Gravitational Force":
+  place.st.markdown("#### science")
+  place.q1 = st.radio("What fundamental force is responsible for the attraction between objects with mass?", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None, key = "q1")
+  place.st.markdown("---")
+  place.q2 = st.radio("Which organelle is often referred to as the 'powerhouse' of the cell because it generates most of the cell's supply of ATP (Adenosine Triphosphate)?", ["A) Nucleus","B) Mitochondrion","C) Endoplasmic Reticulum","D) Golgi Apparatus"], index = None, key = "q2")  
+  place.st.markdown("---")
+  place.q3 = st.radio("What is the chemical symbol for the element Gold?", ["A) Ag","B) Fe","C) Pt","D) Au"], index = None, key = "q3")    
+  place.st.markdown("---")
+  place.q4 = st.radio("What process involves the movement of water from the Earth's surface into the atmosphere, primarily as water vapor?", ["A) Condensation","B) Precipitation","C) Infiltration","D) Evaporation"], index = None, key = "q4")
+  place.st.markdown("---")
+  place.q5 = st.radio("Which of the following is the name given to a small, rocky body orbiting the Sun, typically found in the region between Mars and Jupiter?", ["A) Comet","B) Meteoroid","C) Asteroid","D) Planetoid"], index = None, key = "q5")
+  place.st.markdown("---")
+  if place.q1 == "C) Gravitational Force":
     points += 4
   else:
     points -= 1
-  if q2 == "B) Mitochondrion":
+  if place.q2 == "B) Mitochondrion":
     points += 4
   else:
     points -= 1
-  if q3 == "D) Au":
+  if place.q3 == "D) Au":
     points += 4
   else:
     points -= 1
-  if q4 == "D) Evaporation":
+  if place.q4 == "D) Evaporation":
     points += 4
   else:
     points -= 1
-  if q5 == "C) Asteroid":
+  if place.q5 == "C) Asteroid":
     points += 4
   else:
     points -= 1
   total = points
   revealans = st.button("submit", on_click = reveal_ans)
-  st.markdown(f"### you got {total} points")
+
+def reveal_ans():
+  place.empty
   
   
   
