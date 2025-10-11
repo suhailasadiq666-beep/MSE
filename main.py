89d650ea471aa2c2010259c98ae8b289da030162
suhailasadiq666-beep <suhailasadiq666@gmail.com>
2025-10-11 17:04:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def start_exam():
 
 place = st.empty()
 hold = st.empty()
+tmer = st.empty()
 
   
 
@@ -34,6 +35,12 @@ if st.session_state.head:
   st.markdown("### Time: 35:00 :clock3:")
   start = st.button("start exam" ,on_click = start_exam)
 else:
+  timer = 3100
+  while True:
+    with tmer.container():
+      time.sleep(1)
+      timer -= 1
+      st.markdown(timer)
   with place.container():
     scince = st.markdown("### Science")
     q1 = st.radio("**Q1)What fundamental force is responsible for the attraction between objects with mass?**", ["A) Electromagnetic Force","B) Strong Nuclear Force","C) Gravitational Force","D) Weak Nuclear Force"], index = None, key = "q1")
@@ -255,16 +262,12 @@ else:
         hold.empty()
         hold.markdown(f"### you got {total} points")
 
-tmer = st.empty()
 
-timer = 3100
-while True:
-  with tmer.container():
-    time.sleep(1)
-    timer -= 1
-    st.markdown(timer)
+
+
     
   
+
 
 
 
